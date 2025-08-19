02a4fbb049854417e74276ca723037e49c94b22e
sandrotbilisi <atsetsoffc@gmail.com>
2025-08-19 19:46:11 +0400

    um, kernel driver

---
+++ centipede-driver/src/main.cpp
@@ -0,0 +1,185 @@
+#include <ntifs.h>
+
+extern "C" {
+	NTKERNELAPI NTSTATUS IoCreateDriver(PUNICODE_STRING DriverName, PDRIVER_INITIALIZE InitializationFunction);
+	NTKERNELAPI NTSTATUS MmCopyVirtualMemory(PEPROCESS SourceProcess,
+		PVOID SourceAddress, 
+		PEPROCESS TargetProcess, 
+		PVOID TargetAddress,
+		SIZE_T BufferSize,
+		KPROCESSOR_MODE PreviousMode,
+		PSIZE_T ReturnSize
+	);
+}
+
+void debug_prinnt(PCSTR msg) {
+	KdPrintEx((
+		DPFLTR_IHVDRIVER_ID,
+		DPFLTR_INFO_LEVEL,
+		"Centipede: %s\n",
+		msg
+	));
+}
+
+namespace driver {
+	namespace codes {
+		constexpr ULONG attach = CTL_CODE(FILE_DEVICE_UNKNOWN, 0X696, METHOD_BUFFERED, FILE_SPECIAL_ACCESS);
+		constexpr ULONG read = CTL_CODE(FILE_DEVICE_UNKNOWN, 0X697, METHOD_BUFFERED, FILE_SPECIAL_ACCESS);
+		constexpr ULONG write = CTL_CODE(FILE_DEVICE_UNKNOWN, 0X698, METHOD_BUFFERED, FILE_SPECIAL_ACCESS);
+	}
+	struct Request {
+		HANDLE process_id;
+		PVOID target;
+		PVOID buffer;
+
+		SIZE_T size;
+		SIZE_T return_Size;
+	};
+
+	NTSTATUS create(PDEVICE_OBJECT device_object, PIRP irp) {
+		UNREFERENCED_PARAMETER(device_object);
+
+		IoCompleteRequest(irp, IO_NO_INCREMENT);
+
+		return irp->IoStatus.Status;
+	}
+
+	NTSTATUS close(PDEVICE_OBJECT device_object, PIRP irp) {
+		UNREFERENCED_PARAMETER(device_object);
+
+		IoCompleteRequest(irp, IO_NO_INCREMENT);
+
+		return irp->IoStatus.Status;
+	}
+
+	// Note : Todo
+	NTSTATUS device_control(PDEVICE_OBJECT device_object, PIRP irp) {
+		UNREFERENCED_PARAMETER(device_object);
+
+		debug_prinnt("[+ centipede +] device control called\n");
+
+		NTSTATUS status = STATUS_UNSUCCESSFUL;
+
+		PIO_STACK_LOCATION stack_irp = IoGetCurrentIrpStackLocation(irp);
+
+		auto request = reinterpret_cast<Request*>(irp->AssociatedIrp.SystemBuffer);
+
+		if (stack_irp == nullptr || request == nullptr) {
+			IoCompleteRequest(irp, IO_NO_INCREMENT);
+			return status;
+		}
+
+		static PEPROCESS target_process = nullptr;
+
+		const ULONG control_code = stack_irp->Parameters.DeviceIoControl.IoControlCode;
+
+		switch (control_code) {
+
+			case codes::attach:
+				status = PsLookupProcessByProcessId(request->process_id, &target_process);
+				break;
+			case codes::read:
+				if (target_process != nullptr) {
+					status = MmCopyVirtualMemory(
+						target_process,
+						request->target,
+						PsGetCurrentProcess(),
+						request->buffer,
+						request->size,
+						KernelMode,
+						&request->return_Size
+					);
+				}
+
+				break;
+
+			case codes::write:
+
+				status = MmCopyVirtualMemory(
+					PsGetCurrentProcess(),
+					request->buffer,
+					target_process,
+					request->target,
+					request->size,
+					KernelMode,
+					&request->return_Size
+				);
+
+
+				break;
+
+			default:
+				break;
+
+		}
+
+		irp->IoStatus.Status = status;
+		irp->IoStatus.Information = sizeof(Request);
+
+		IoCompleteRequest(irp, IO_NO_INCREMENT);
+
+		return irp->IoStatus.Status;
+	}
+}
+
+NTSTATUS driver_main(PDRIVER_OBJECT driver_object, PUNICODE_STRING registry_path) {
+	UNREFERENCED_PARAMETER(registry_path);
+
+	// create device_name string
+	UNICODE_STRING device_name = {};
+	RtlInitUnicodeString(&device_name, L"\\Device\\centipede");
+
+	 // create driver ojbect
+	PDEVICE_OBJECT device_object = nullptr;
+	NTSTATUS status = IoCreateDevice(driver_object, 0, &device_name, FILE_DEVICE_UNKNOWN, FILE_DEVICE_SECURE_OPEN, FALSE, &device_object);
+
+	// handle if device wasnt created
+	if (status != STATUS_SUCCESS) {
+		debug_prinnt("[-] Failed To Create Driver Device\n");
+		return status;
+	}
+
+	debug_prinnt("[+] Driver Device Created Succesfully!");
+
+	UNICODE_STRING symbolic_link = {};
+	RtlInitUnicodeString(&symbolic_link, L"\\DosDevices\\centipede");
+
+	status = IoCreateSymbolicLink(&symbolic_link, &device_name);
+
+	if (status != STATUS_SUCCESS) {
+		debug_prinnt("[-] Failed To Create Driver Symbolic Link\n");
+		return status;
+	}
+
+
+	debug_prinnt("[+] Driver Symbolic Link Created Succesfully!");
+
+
+	SetFlag(device_object->Flags, DO_BUFFERED_IO);
+
+	driver_object->MajorFunction[IRP_MJ_CREATE] = driver::create;
+	driver_object->MajorFunction[IRP_MJ_CLOSE] = driver::close;
+	driver_object->MajorFunction[IRP_MJ_DEVICE_CONTROL] = driver::device_control;
+
+	ClearFlag(device_object->Flags, DO_BUFFERED_IO);
+
+	debug_prinnt("[+] Driver Initialized Succesfully!");
+
+
+
+	return status;
+}
+
+NTSTATUS DriverEntry() {
+	debug_prinnt("[+] centepede driver loaded");
+
+	// initialize empty driver name
+	UNICODE_STRING driver_name = {};
+
+	// fill driver_name with unicode
+	RtlInitUnicodeString(&driver_name, L"\\Driver\\centipede");
+
+
+
+	return IoCreateDriver(&driver_name,&driver_main);
+}
---
+++ um/src/main.cpp
@@ -0,0 +1,136 @@
+#include <iostream>
+#include <Windows.h>
+#include <TlHelp32.h>
+
+#include <Windows.h>
+#include <TlHelp32.h>
+#include <cstdint>
+
+// Get the process ID by name
+static DWORD get_process_id(const wchar_t* process_name) {
+	DWORD process_id = 0;
+	HANDLE snap_shot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	if (snap_shot == INVALID_HANDLE_VALUE)
+		return 0;
+
+	PROCESSENTRY32W entry = {};
+	entry.dwSize = sizeof(entry);
+
+	if (Process32FirstW(snap_shot, &entry)) {
+		do {
+			if (_wcsicmp(process_name, entry.szExeFile) == 0) {
+				process_id = entry.th32ProcessID;
+				break;
+			}
+		} while (Process32NextW(snap_shot, &entry));
+	}
+
+	CloseHandle(snap_shot);
+	return process_id;
+}
+
+// Get the base address of a module in a process
+static std::uintptr_t get_module_base(const DWORD pid, const wchar_t* module_name) {
+	std::uintptr_t module_base = 0;
+
+	// Use bitwise OR, not logical ORa
+	HANDLE snap_shot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, pid);
+	if (snap_shot == INVALID_HANDLE_VALUE)
+		return 0;
+
+	MODULEENTRY32W entry = {};
+	entry.dwSize = sizeof(entry);
+
+	if (Module32FirstW(snap_shot, &entry)) {
+		do {
+			if (_wcsicmp(module_name, entry.szModule) == 0) {
+				module_base = reinterpret_cast<std::uintptr_t>(entry.modBaseAddr);
+				break;
+			}
+		} while (Module32NextW(snap_shot, &entry));
+	}
+
+	CloseHandle(snap_shot);
+	return module_base;
+}
+
+namespace driver {
+	namespace codes {
+		constexpr ULONG attach = CTL_CODE(FILE_DEVICE_UNKNOWN, 0X696, METHOD_BUFFERED, FILE_SPECIAL_ACCESS);
+		constexpr ULONG read = CTL_CODE(FILE_DEVICE_UNKNOWN, 0X697, METHOD_BUFFERED, FILE_SPECIAL_ACCESS);
+		constexpr ULONG write = CTL_CODE(FILE_DEVICE_UNKNOWN, 0X698, METHOD_BUFFERED, FILE_SPECIAL_ACCESS);
+	}
+	struct Request {
+		HANDLE process_id;
+		PVOID target;
+		PVOID buffer;
+
+		SIZE_T size;
+		SIZE_T return_Size;
+	};
+
+	bool attach_to_process(HANDLE driver_handle, const DWORD pid) {
+		Request r;
+		r.process_id = reinterpret_cast<HANDLE>(pid);
+
+		return DeviceIoControl(driver_handle, codes::attach, &r, sizeof(r), &r, sizeof(r), nullptr, nullptr);
+	}
+
+	template <class T>
+	T read_memory(HANDLE driver_handle, const std::uintptr_t addr) {
+		T temp = {};
+		Request r;
+		r.target = reinterpret_cast<PVOID>(addr);
+		r.buffer = &temp;
+		r.size = sizeof(temp);
+
+		DeviceIoControl(driver_handle, codes::read, &r, sizeof(r), &r, sizeof(r), nullptr, nullptr);
+
+		return temp;
+
+	}
+
+	template <class T>
+	void write_memory(HANDLE driver_handle, const std::uintptr_t addr, const T& value) {
+		Request r;
+		r.target = reinterpret_cast<PVOID>(addr);
+		r.buffer = (PVOID)&value;
+		r.size = sizeof(T);
+
+
+		DeviceIoControl(driver_handle, codes::write, &r, sizeof(r), &r, sizeof(r), nullptr, nullptr);
+
+	}
+
+
+}
+
+int main() {
+
+	const DWORD pid = get_process_id(L"notepad.exe");
+
+	if (pid == 0) {
+		std::cout << "Failed To Find Roblox Open\n";
+		std::cin.get();
+		return 1;
+	}
+
+	std::cout << "found instance\n";
+
+	const HANDLE driver = CreateFile(L"\\\\.\\centipede", GENERIC_READ, 0, nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr);
+
+	if (driver == INVALID_HANDLE_VALUE) {
+		std::cout << "Failed To Create Driver Handle\n";
+		std::cin.get();
+		return 1;
+	}
+
+	if (driver::attach_to_process(driver, pid) == true) 
+		std::cout << "Successfully Attached To Roblox\n";
+
+	CloseHandle(driver);
+
+
+	std::cout << "hello world" << std::endl;
+	return 0;
+}
