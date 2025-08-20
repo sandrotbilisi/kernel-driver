ae855ec12217936485b54676898e9a1fb5375022
sandrotbilisi <atsetsoffc@gmail.com>
2025-08-20 18:25:04 +0400

    addresses3

--- um/src/main.cpp
+++ um/src/main.cpp
@@ -77,7 +77,7 @@ namespace driver {
 		return DeviceIoControl(driver_handle, codes::attach, &r, sizeof(r), &r, sizeof(r), nullptr, nullptr);
 	}
 
-	template <class T>
+		template <class T>
 	T read_memory(HANDLE driver_handle, const std::uintptr_t addr) {
 		T temp = {};
 		Request r;
@@ -85,10 +85,15 @@ namespace driver {
 		r.buffer = &temp;
 		r.size = sizeof(temp);
 
-		DeviceIoControl(driver_handle, codes::read, &r, sizeof(r), &r, sizeof(r), nullptr, nullptr);
-
+		DWORD bytes_returned = 0;
+		BOOL result = DeviceIoControl(driver_handle, codes::read, &r, sizeof(r), &r, sizeof(r), &bytes_returned, nullptr);
+		
+		if (!result) {
+			DWORD error = GetLastError();
+			std::cout << "Read failed at address 0x" << std::hex << addr << std::dec << " Error: " << error << std::endl;
+		}
+		
 		return temp;
-
 	}
 
 	template <class T>
@@ -135,16 +140,45 @@ int main() {
 		std::uintptr_t roblox_base = get_module_base(pid, L"RobloxPlayerBeta.exe");
 
 		std::cout << "Roblox Base: " << std::hex << roblox_base << std::dec << "\n";
+		std::cout << "VRealDataModel offset: 0x" << std::hex << dump::VRealDataModel << std::dec << "\n";
+		std::cout << "Calculated address: 0x" << std::hex << (roblox_base + dump::VRealDataModel) << std::dec << "\n";
 
+		// Test read at base address to verify driver is working
+		std::uint32_t test_read = driver::read_memory<std::uint32_t>(driver, roblox_base);
+		std::cout << "Test read at base: 0x" << std::hex << test_read << std::dec << "\n";
 
 		std::uintptr_t data_model = driver::read_memory<std::uintptr_t>(driver, roblox_base + dump::VRealDataModel);
 		std::cout << "Data Model: " << std::hex << data_model << std::dec << "\n";
+		std::cout << "Data Model Address: 0x" << std::hex << (roblox_base + dump::VRealDataModel) << std::dec << "\n";
+
+		if (data_model == 0) {
+			std::cout << "ERROR: Data Model is 0, cannot continue!\n";
+			CloseHandle(driver);
+			std::cin.get();
+			return 1;
+		}
 
 		std::uintptr_t player_service = driver::read_memory<std::uintptr_t>(driver, data_model + dump::DPlayerService);
 		std::cout << "Player Service: " << std::hex << player_service << std::dec << "\n";
+		std::cout << "Player Service Address: 0x" << std::hex << (data_model + dump::DPlayerService) << std::dec << "\n";
+
+		if (player_service == 0) {
+			std::cout << "ERROR: Player Service is 0!\n";
+			CloseHandle(driver);
+			std::cin.get();
+			return 1;
+		}
 
 		std::uintptr_t local_player = driver::read_memory<std::uintptr_t>(driver, player_service + dump::PLocalPlayer);
 		std::cout << "Local Player: " << std::hex << local_player << std::dec << "\n";
+		std::cout << "Local Player Address: 0x" << std::hex << (player_service + dump::PLocalPlayer) << std::dec << "\n";
+
+		if (local_player == 0) {
+			std::cout << "ERROR: Local Player is 0!\n";
+			CloseHandle(driver);
+			std::cin.get();
+			return 1;
+		}
 
 		std::uintptr_t player_health = driver::read_memory<std::uintptr_t>(driver, local_player + dump::HHealth);
 		std::cout << "Player Health: " << std::hex << player_health << std::dec << "\n";
